06d34cecb8e665535eb753ed99297e81e132a452
Vincent Atmadja <vincentpatmadja@gmail.com>
2022-06-18 23:20:12 +0700

    Setup firebase and mongodb

--- server.js
+++ server.js
@@ -4,18 +4,24 @@ const cors = require('cors');
 
 require('dotenv').config();
 
+//Setup express
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI
+
+//Setup mongo connection
 mongoose.connect(uri, { 
     useNewUrlParser: true
 });
 
 const connection = mongoose.connection;
+
 connection.once('open', () => {
     console.log("MongoDB succesfully connected");
 })
+
+connection.on('error', console.error.bind(console, 'MongoDB connection error: '))
 
 app.use(cors());
 app.use(express.json());
