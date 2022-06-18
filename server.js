bcec64293587312ae35baf47e98a4c0797012098
Vincent Atmadja <vincentpatmadja@gmail.com>
2022-06-18 15:13:03 +0700

    Init server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { 
+    useNewUrlParser: true
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB succesfully connected");
+})
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port} `);
+});
